33ed7061f9a50db294573713b0d1dea862abda79
Daniele Iudica <102958741+DanieleIudica@users.noreply.github.com>
2022-09-19 13:10:02 +0200

    first_commit_navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Navbar } from "react-bootstrap";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NotFound from "./components/NotFound";
 import { User } from "./components/User";
+import { Navbar } from "./components/Navbar";
 
 function App() {
     return (

9fea89bf5ecc3904a3b32b5cfa9958d4a3983af0
Gaetano Giulio Notarangelo <102958789+Gaeta313@users.noreply.github.com>
2022-09-21 12:34:52 +0200

    feedfooter completo

--- src/components/RightFeed.jsx
+++ src/components/RightFeed.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import FeedFooter from "./FeedFooter";
 
 export const RightFeed = () => {
     const me = useSelector((state) => state.user.me);
     const theme = useSelector((state) => state.user.theme);
     const styles = useSelector((state) => state.user.styles);
     return (
+        <div>
+
         <div className="feedDiv" style={theme ? styles.light : styles.dark}>
             RightFeed
+        </div>
+            <FeedFooter />
         </div>
     );
 };
